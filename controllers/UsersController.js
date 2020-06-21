8b2dfa59578dd61b8db326f5c41c725ca4c9e90e
kinga sadowska <kingaasadowska@gmail.com>
2020-06-21 13:47:56 +0100

    BK route protection

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -75,7 +75,26 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({user: createdUser.toObject({ getters: true })});
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      'Register failed.',
+      500
+  );
+  return next(error);
+}
+
+  res.status(201).json({
+    userId: createdUser.id, 
+    email: createdUser.email, 
+    token: token
+  });
 };
 
 const login = async(req, res, next) => {
@@ -117,9 +136,25 @@ const login = async(req, res, next) => {
     return next(error);
   }
 
-  res.json(
-    {message: 'Success!',
-    user: existingUser.toObject({ getters: true })
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      'Register failed.',
+      500
+  );
+  return next(error);
+}
+
+  res.json({
+    userId: existingUser.id, 
+    email: existingUser.email, 
+    token: token
   });
 };
 
